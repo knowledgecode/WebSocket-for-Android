9e75963ba08948531e462b531dd35e207a7f9df5
knowledgecode <knowledgecode@gmail.com>
2014-03-16 22:03:01 +0900

    removed a second argument from the send() method

--- assets/www/websocket.js
+++ assets/www/websocket.js
@@ -133,11 +133,11 @@
             this.OPEN = 1;
             this.CLOSING = 2;
             this.CLOSED = 3;
-            this.send = function (data, asBinary) {
+            this.send = function (data) {
                 var that = this;
 
                 if (typeof data === 'string') {
-                    exec(null, null, 'WebSocket', 'send', [this.__getId__(), data, !!asBinary]);
+                    exec(null, null, 'WebSocket', 'send', [this.__getId__(), data, false]);
                 } else {
                     binaryToString(data, function (blob) {
                         exec(null, null, 'WebSocket', 'send', [that.__getId__(), blob, true]);
