c178dd1afcb6cbd1a5e8345e2280e69d1c39bc04
knowledgecode <knowledgecode@gmail.com>
2015-11-28 17:27:37 +0900

    Bump version

--- www/websocket.js
+++ www/websocket.js
@@ -19,7 +19,7 @@
 /**
  * Cordova WebSocket Plugin for Android
  * @author KNOWLEDGECODE <knowledgecode@gmail.com>
- * @version 0.11.1
+ * @version 0.12.0
  */
 (function (window) {
     'use strict';
